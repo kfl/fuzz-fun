78b1bb7595fe70f4301449ebc26c0c357c1aa4f4
Ken Friis Larsen <ken@friislarsen.net>
2018-04-09 16:01:16 +0200

    Add Seasoned Software script

---
+++ seasoned-software.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Print environment for debuggin.
+# env
+
+# Cargo-fuzz needs nightly rust, so switch this project to nightly
+rustup override set nightly
+
+# Build and register all tests known to Cargo-fuzz.
+for t in $(cargo fuzz list|sed 's@\x1b[^m]*m@@g'); do
+	echo "Building test: $t"
+	cargo fuzz run $t --release -- -help=1
+	exe="$(find fuzz/target -iname $t -executable)"
+
+	echo "Registering: $exe"
+	register-binary "$t" "$exe"
+done
